59b2fe86ec3461ac170a9a43d9df1e2446f3a33f
Rakesh Maurya <rakeshmauryamail2@gmail.com>
2026-09-17 20:39:54 +0530

    Fixed syntex error

--- backend-new/server.js
+++ backend-new/server.js
@@ -113,5 +113,5 @@ app.get("/api/quotes", async (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(Filter Group Solar backend running on port ${PORT});
+  console.log(`Filter Group Solar backend running on port ${PORT}`);
 });

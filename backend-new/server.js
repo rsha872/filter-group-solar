ee169f96e231c8ae2e31320a37e8034454a29ff5
Rakesh Maurya <rakeshmauryamail2@gmail.com>
2026-09-17 19:59:07 +0530

    Fix Railway database connection

--- backend-new/server.js
+++ backend-new/server.js
@@ -11,11 +11,8 @@ app.use(express.json());
 
 /* PostgreSQL Connection */
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString;
+process.env.Database_URL,
 });
 
 /* Test Database Connection */
